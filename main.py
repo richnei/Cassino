9658d8d4669712adebed9fea68ada132f3c0af5d
RichNei <rich.nei@hotmail.com>
2023-11-27 13:38:48 -0300

    class

--- main.py
+++ main.py
@@ -4,3 +4,17 @@ from pydantic import BaseModel
 app = FastAPI()
 
 
+class Bet(BaseModel):
+    player: int
+    value: float
+
+
+class Win(BaseModel):
+    player: int
+    value: float
+
+
+class Rollback(BaseModel):
+    player: int
+    txn: int
+    value: float
