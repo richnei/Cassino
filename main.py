87016d3d39c39287df805e1bbda9495c57d49531
RichNei <rich.nei@hotmail.com>
2023-11-27 14:44:01 -0300

    request_win

--- main.py
+++ main.py
@@ -39,3 +39,15 @@ def place_bet(bet: Bet):
     transactions[txn_id] = {"player": bet.player, "value": -bet.value}
     
     return {"player": bet.player, "balance": player_balances[bet.player], "txn": txn_id}
+
+@app.post("/win")
+def win_game(win: Win):
+    if win.player not in player_balances:
+        raise HTTPException(status_code=400, detail="Player não existe")
+    
+    txn_id = len(transactions) + 1
+    transactions[txn_id] = {"player": win.player, "value": win.value}
+
+    player_balances[win.player] += win.value
+
+    return {"player": win.player, "balance": player_balances[win.player], "txn": txn_id}
