c120aeb5e9144ee1c3492a4ab02d9a109a447eae
RichNei <rich.nei@hotmail.com>
2023-11-27 13:58:46 -0300

    request_bet

--- main.py
+++ main.py
@@ -27,3 +27,15 @@ class Rollback(BaseModel):
 def get_balance(player: int):
     balance = player_balances.get(player, 1000)
     return {"player": player, "balance": balance}
+
+@app.post("/bet")
+def place_bet(bet: Bet):
+    if bet.player not in player_balances:
+        player_balances[bet.player] = 1000 
+
+    player_balances[bet.player] -= bet.value
+
+    txn_id = len(transactions) + 1
+    transactions[txn_id] = {"player": bet.player, "value": -bet.value}
+    
+    return {"player": bet.player, "balance": player_balances[bet.player], "txn": txn_id}
